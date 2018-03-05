02faf0531132f054eb009a31af343a5722959878
Kristin Agcaoili <kagcaoil@ucsd.edu>
2018-03-04 19:10:20 -0800

    Added more ghosts and instantiation and moving toward player

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour {
+
+    private Vector3 playerPosition;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float minDistance;
+
+	// Use this for initialization
+	void Start () {
+        playerPosition = Camera.main.transform.position;
+    }
+	
+    private void FloatToUser()
+    {
+        playerPosition = Camera.main.transform.position;
+        float distance = Vector3.Distance(transform.position, playerPosition);
+
+        Debug.Log("Distance: " + distance);
+        if (distance > minDistance)
+        {
+            Debug.Log("Fly to user");
+            Vector3 direction = playerPosition - transform.position;
+            direction.Normalize();
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        FloatToUser();
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public enum Mode
+    {
+        Easy,
+        Hard
+    };
+
+
+    public GameObject[] easyGhosts;
+
+    public GameObject[] hardGhosts;
+
+    private GameObject[] ghosts;
+
+    [SerializeField]
+    private int maxGhosts;
+
+    [SerializeField]
+    private Mode mode;
+
+    private int numberOfExistingGhosts;
+
+	// Use this for initialization
+	void Start () {
+        if (mode == Mode.Easy)
+        {
+            ghosts = easyGhosts;
+        } else
+        {
+            ghosts = hardGhosts;
+        }
+
+
+        numberOfExistingGhosts = 0;
+        SpawnGhosts(maxGhosts);
+	}
+
+    private void SpawnGhosts(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            int index = Random.Range(0, ghosts.Length);
+            Instantiate(ghosts[index], Vector3.zero, Quaternion.identity);
+            numberOfExistingGhosts++;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
